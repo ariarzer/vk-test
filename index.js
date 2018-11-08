3c1654b4402676f9d7c5d8afc526d74eb80b1470
Sofi Valitova <ariarzer@gmail.com>
2018-11-08 18:39:41 +0300

    Linter is configured

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require('http');
 
 const server = new http.Server();
 
-server.on('request', function (req, res) {
-    res.end('Hello World!');
+server.on('request', (req, res) => {
+  res.end('Hello World!');
 });
 
 server.listen(process.env.PORT || 8080);
