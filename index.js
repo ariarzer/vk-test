5537a92f60437cb644ecf1bbd3d0ab809d34d3f8
Sofi Valitova <ariarzer@gmail.com>
2018-11-11 20:11:50 +0300

    Change a data format

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const http = require('http');
-
+const url = require('url');
 const express = require('express');
 
+const buildTree = require('./utils/buildTree.js');
+const find = require('./utils/find.js');
+
+const users = require('./data/users.json');
+
+const names = {};
+Object.keys(users).map((key) => {
+  names[key] = users[key].personalName;
+  return 0;
+});
+
+const namesTree = buildTree(names);
+
 const app = express();
-
-http.createServer(app);
-
-app.listen(process.env.PORT || 8080);
 
 app.use((req, res, next) => {
   console.log(req.url);
@@ -16,5 +24,8 @@ app.use((req, res, next) => {
 app.use(express.static('static'));
 
 app.get('/search', (req, res) => {
-  res.send({ text: 'Hello' });
+  const { value } = url.parse(req.url, true).query;
+  res.send(find(namesTree, users, value));
 });
+
+app.listen(process.env.PORT || 8080);
