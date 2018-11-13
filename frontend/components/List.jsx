53347aaa2b1f74b52dea6ee04f620b8e0b26d863
Sofi Valitova <ariarzer@gmail.com>
2018-11-13 19:34:44 +0300

    Now the search is performed by personal names and family names (family names priority)

--- frontend/components/List.jsx
+++ frontend/components/List.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function List(props) {
   return (
     <div>
-      <ul>{props.searchResult.map((item) => (<li>{item.familyName}</li>))}</ul>
+      <ul>{props.searchResult.map((item) => (<li>{item.personalName}  {item.familyName}</li>))}</ul>
     </div>
   );
 }
