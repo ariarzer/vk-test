295d4fa940004b3f12f490e87bb6d7660c83a3ea
Sofi Valitova <ariarzer@gmail.com>
2018-11-21 10:33:20 +0300

    Rename main component Dropdown to index.jsx

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
 
-import Dropdown from './Dropdown/Dropdown.jsx';
+import Dropdown from './Dropdown/index.jsx';
 
 const config = {
   multiple: true,
